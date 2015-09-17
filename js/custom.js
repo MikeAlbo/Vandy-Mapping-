de31f6e37be4e3250cd3877c5d6f46eca2a519bc
Michael Albonetti <malbo24@gmail.com>
2015-09-17 14:09:36 -0500

    update map center

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 // setup map
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([36.142000, -86.802843], 15);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
